ef0f6e1fba8f6086d80d53fe9ad38f091bbf2162
Kyle Pinheiro <23409579+kylepin@users.noreply.github.com>
2022-01-23 13:03:00 -0500

    cleanup and comments

---
+++ train_and_cv.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+#
+# TO RUN: sbatch train_and_cv.sh
+
+# the end value must be equal to one less than the number of runs
+#SBATCH --array=0-55%16
+#SBATCH --mail-type=END
+#SBATCH --mail-user=<email here>
+
+outdir="" # specify path for data output (must already exist)
+
+models=("support_vector_regression"  "random_forest_regression")
+
+experiments=("ets1_ets1" "ets1_runx1")
+
+feature_sets=(
+"distance"
+"affinity"
+"orientation"
+"distance,affinity"
+"distance,orientation"
+"distance,affinity,orientation"
+"distance,affinity,orientation,shape_in,shape_out"
+"distance,affinity,orientation,sequence_in,sequence_out"
+"position"
+"position,affinity"
+"position,orientation"
+"position,affinity,orientation"
+"position,affinity,orientation,shape_in,shape_out"
+"position,affinity,orientation,sequence_in,sequence_out"
+)
+
+
+# for loop over models, feature sets, and models
+args=()
+for model in "${models[@]}"; do
+  for experiment in "${experiments[@]}"; do
+    for feature_set in "${feature_sets[@]}"; do
+      args+=("${experiment} ${model} ${feature_set}")
+    done
+  done
+done
+
+#echo ${#args[@]}
+
+# Use correct paths to appropriate python installation and python file
+srun python cooperativity-prediction/automate_grid_search.py \
+"${SLURM_ARRAY_TASK_ID}" "${outdir}" "${args[${SLURM_ARRAY_TASK_ID}]}"
